4630d093b071b6f15d71bebce1db05600dbf29b9
GD <63779654+gd03champ@users.noreply.github.com>
2022-08-29 21:21:38 +0530

    Update call.py

--- call.py
+++ call.py
@@ -55,6 +55,6 @@ if (serial.inWaiting()>0):
   map_link = f"https://www.google.com/maps/search/?api=1&query={lat}%2C{lon}"
   content = f"{name} is in the location of {location} and might need your help!. Google map ink {map_link}"
 
-  cmd = '''curl -X POST -H "Authorization: Bearer cbedd4e12e034f279447c4d617afe28f" -H "Content-Type: application/json" -d '{ "from": "447520651713", "to": [ "918825648984" ], "body": " '''+ content + '''" }' "https://sms.api.sinch.com/xms/v1/f85ebdfc9c5048a18ce4196276e60827/batches" '''
+  cmd = '''curl -X POST -H "Authorization: Bearer cbedd4e12e034f279447c4GD17afe28f" -H "Content-Type: application/json" -d '{ "from": "447520651700", "to": [ "918825648984" ], "body": " '''+ content + '''" }' "https://sms.api.sinch.com/xms/v1/f85ebdfc9c5048a18GD4196276e60827/batches" '''
 
   os.system(cmd)
